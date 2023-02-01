3a3f5b3de5c3a002b373c00b75828a29d32dfa42
Jonathan <jonathan.j.curtis@outlook.com>
2023-02-01 13:30:55 -0600

    Add global functions

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+let count = 0;
+let values = [];
+let getNumber = () => parseFloat(display.textContent);
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+const clear = () => display.textContent = '';
+
 // ------ SELECTORS ------
 
 let buttons = document.querySelectorAll('button');
-let numbers = document.querySelectorAll('.numbers button')
+let numbers = document.querySelectorAll('.numbers button');
+let operations = document.querySelectorAll('.basic-operations button');
 let display = document.querySelector('.display');
-
-//============================================//
-
+let sum = document.querySelector('.add');
+let equals = document.querySelector('#equals');
 
 // ------ LISTENERS ------
 
@@ -25,4 +34,13 @@ numbers.forEach(number => {
     number.addEventListener('click', () => {
         display.textContent += number.textContent;
     })
+})
+
+sum.addEventListener('click', () => {
+   
+    return values;
+})
+
+equals.addEventListener('click', () => {
+    
 })
