8294925ce498731ac5e1e739b542b5bd4be601c2
Jonathan <jonathan.j.curtis@outlook.com>
2023-02-02 23:47:40 -0600

    Refactor global memory and insert switch statement
    Need to fix bug introduced by clear button.

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let count = 0;
-let values = [];
-let getNumber = () => parseFloat(display.textContent);
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
-const clear = () => display.textContent = '';
+let num2;
+let total;
+let displayValue;
+let operator;
+const clearDisplay = () => display.textContent = '';
 
 // ------ SELECTORS ------
 
@@ -13,7 +11,11 @@ let buttons = document.querySelectorAll('button');
 let numbers = document.querySelectorAll('.numbers button');
 let operations = document.querySelectorAll('.basic-operations button');
 let display = document.querySelector('.display');
+let clear = document.querySelector('.clear');
 let sum = document.querySelector('.add');
+let subtract = document.querySelector('.subtract');
+let multiply = document.querySelector('.multiply');
+let divide = document.querySelector('.divide');
 let equals = document.querySelector('#equals');
 
 // ------ LISTENERS ------
@@ -33,14 +35,72 @@ buttons.forEach(button => {
 numbers.forEach(number => {
     number.addEventListener('click', () => {
         display.textContent += number.textContent;
+        displayValue = (+display.textContent);
+        if (count == 0) {
+            total = displayValue;
+        } else {
+            num2 = displayValue;
+        }
+
+        switch (operator) {
+            case 1:
+                total += num2;
+                break;
+            case 2:
+                total -= num2;
+                break;
+            case 3:
+                total *= num2;
+                break;
+            case 4:
+                total /= num2;
+                break;    
+            default:
+                break;
+        }
     })
 })
 
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        count = 1;
+        return total;
+    })
+})
+
+clear.addEventListener('click', () => {
+    clearDisplay();
+    count = 0;
+    total = undefined;
+    num2 = undefined;
+    return count;
+})
+
 sum.addEventListener('click', () => {
-   
-    return values;
+    clearDisplay();
+    operator = 1;
+    return operator;
+})
+
+subtract.addEventListener('click', () => {
+    clearDisplay();
+    operator = 2;
+    return operator;
+})
+
+multiply.addEventListener('click', () => {
+    clearDisplay();
+    operator = 3;
+    return operator;
+})
+
+divide.addEventListener('click', () => {
+    clearDisplay();
+    operator = 4;
+    return operator;
 })
 
 equals.addEventListener('click', () => {
-    
+    clearDisplay();
+    display.textContent = total;
 })
